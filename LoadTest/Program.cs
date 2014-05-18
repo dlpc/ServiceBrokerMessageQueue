043a2f55f450b80bc4a67dfe3bf97509c27b0efb
AdrianRussell <adrian.f.russell@gmail.com>
2014-05-19 09:15:04 +1000

    Add load test console app.

---
+++ LoadTest/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LoadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //parse args
+
+            //check database
+
+            //check message queue
+
+            //do message queue test
+        }
+    }
+}
