0bafac059ea5de374abe4bbad9a976f5d9be6881
AdrianRussell <adrian.f.russell@gmail.com>
2014-05-20 11:21:50 +1000

    add in load test writing to DB & MSMQ and writing to DB standalone.

--- LoadTest/Program.cs
+++ LoadTest/Program.cs
@@ -1,16 +1,132 @@
-﻿namespace LoadTest
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Messaging;
+using System.Threading;
+using System.Transactions;
+
+namespace LoadTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //parse args
 
             //check database
 
             //check message queue
+              string messageQueueName = args[2];
 
-            //do message queue test
+            const int value = 1;
+
+            for (var i = 0; i < 10; i++)
+            {
+                var thread = new Thread(() => WriteToQueues(messageQueueName, value));
+                thread.Start();
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                var thread = new Thread(() => WriteToDatabase(messageQueueName, value));
+                thread.Start();
+            }
+
+
         }
+
+        private static void WriteToQueues(string messageQueueName, int value)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            const int numberOfExecutions = 1000;
+            for (int i = 0; i < numberOfExecutions; i++)
+            {
+                WriteToQueueAndDatabase(messageQueueName, value);
+            }
+            sw.Stop();
+
+            Console.WriteLine("{0} entries written to MSMQ and Database in {1} ms", numberOfExecutions,
+                sw.ElapsedMilliseconds);
+        }
+
+        private static void WriteToDatabase(string messageQueueName, int value)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            const int numberOfExecutions = 1000;
+            for (int i = 0; i < numberOfExecutions; i++)
+            {
+                WriteToDatabase(value);
+            }
+            sw.Stop();
+
+            Console.WriteLine("{0} entries written to Database in {1} ms", numberOfExecutions,
+                sw.ElapsedMilliseconds);
+        }
+
+        
+
+        private static void WriteToQueueAndDatabase(string messageQueueName, int value)
+        {
+            using (var scope = new TransactionScope())
+            {
+                string msmq = string.Format(@".\private$\{0}", messageQueueName);
+                var messageQueue = new MessageQueue(msmq);
+                messageQueue.Send("sds", MessageQueueTransactionType.Automatic);
+
+                var connection = new SqlConnection(
+                    ConfigurationManager.ConnectionStrings["DB"].ToString());
+                connection.Open();
+                var cmd = new SqlCommand
+                {
+                    Connection = connection,
+                    CommandType = CommandType.Text,
+                    CommandText = @"Insert into test_table            ([key]
+                    ,[data])values (@key,@data)"
+                };
+
+                cmd.Parameters.AddWithValue("key", Guid.NewGuid().ToString());
+                cmd.Parameters.AddWithValue("data", value);
+
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                scope.Complete();
+            }
+        }
+
+        private static void WriteToDatabase(int value)
+        {
+            using (var scope = new TransactionScope())
+            {
+
+
+                var connection = new SqlConnection(
+                    ConfigurationManager.ConnectionStrings["DB"].ToString());
+                connection.Open();
+                var cmd = new SqlCommand
+                {
+                    Connection = connection,
+                    CommandType = CommandType.Text,
+                    CommandText = @"Insert into test_table            ([key]
+                    ,[data])values (@key,@data)"
+                };
+
+                cmd.Parameters.AddWithValue("key", Guid.NewGuid().ToString());
+                cmd.Parameters.AddWithValue("data", value);
+
+                cmd.ExecuteNonQuery();
+
+                cmd.Parameters["key"].Value = Guid.NewGuid().ToString();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                scope.Complete();
+            }
+        }
+
     }
 }
--- src/MessageQueue/MessageQueue/ServiceBrokerMessageQueue.cs
+++ src/MessageQueue/MessageQueue/ServiceBrokerMessageQueue.cs
@@ -57,6 +57,7 @@ namespace MessageQueue
             }
             finally
             {
+                
                 _connection.Close();
             }
         }
