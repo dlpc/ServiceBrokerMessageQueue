9cf7450b3e658f753e874576e2a08bc3ded26947
AdrianRussell <adrian.f.russell@gmail.com>
2014-05-29 08:10:01 +1000

    shell for send_message stored proc

---
+++ src/Database/Procs/send_message.sql
@@ -0,0 +1,53 @@
+IF EXISTS ( SELECT  *
+            FROM    sys.objects
+            WHERE   object_id = OBJECT_ID(N'[message_queue].[send_message]')
+                    AND type IN ( N'P', N'PC' ) ) 
+BEGIN
+    DROP PROCEDURE [message_queue].[send_message];
+END ;
+GO
+
+CREATE PROCEDURE [message_queue].[send_message]
+	@from_service nvarchar(50)
+	@to_service nvarchar(50)
+	@message XML
+
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SET XACT_ABORT,
+        QUOTED_IDENTIFIER,
+        ANSI_NULLS,
+        ANSI_PADDING,
+        ANSI_WARNINGS,
+        ARITHABORT,
+        CONCAT_NULL_YIELDS_NULL ON;
+    SET NUMERIC_ROUNDABORT OFF;
+ 
+    DECLARE @localTran bit
+    IF @@TRANCOUNT = 0
+    BEGIN
+        SET @localTran = 1
+        BEGIN TRANSACTION LocalTran
+    END
+ 
+    BEGIN TRY
+    END TRY
+    BEGIN CATCH
+ 
+        DECLARE @ErrorMessage NVARCHAR(4000)
+        DECLARE @ErrorSeverity INT
+        DECLARE @ErrorState INT
+ 
+        SELECT  @ErrorMessage = ERROR_MESSAGE(),
+                @ErrorSeverity = ERROR_SEVERITY(),
+                @ErrorState = ERROR_STATE()
+ 
+        IF @localTran = 1 AND XACT_STATE() <> 0
+            ROLLBACK TRAN
+ 
+        RAISERROR ( @ErrorMessage, @ErrorSeverity, @ErrorState)
+ 
+    END CATCH
+
+END
